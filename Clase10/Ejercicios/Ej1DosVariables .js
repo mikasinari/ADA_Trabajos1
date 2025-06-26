52c6ecf6add26378815fd24bf7f7e3326ad290f9
mikasinari <mikasinari@gmail.com>
2025-06-25 23:22:08 -0300

    Agregué Ej1DosVariables.js

---
+++ Clase10/Ejercicios/Ej1DosVariables .js
@@ -0,0 +1,9 @@
+//declarar dos variables: una con el nombre edad 
+//y la otra con el nombre peso. 
+//Después deberás asignarle un valor.
+//Recordemos que para crear una variable debemos utilizar
+//la palabra reservada "let", seguida del nombre con el cual luego le haremos referencia.
+
+let edad = 25; // Asignando un valor a la variable edad
+let peso = 70; // Asignando un valor a la variable peso
+
